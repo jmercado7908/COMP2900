d005beb8bff617fd226e9e4570e07255d1000efe
Johnny Mercado <johnny@Johnnys-Air.arecibo.inter.edu>
2024-03-21 13:22:56 -0400

    Update

--- pruebas_tiempo/leccion/ejemplo01.py
+++ pruebas_tiempo/leccion/ejemplo01.py
@@ -13,8 +13,17 @@ def factorial_recursivo(n):
         return n * factorial_recursivo(n-1)
     
 # Tiempo del enfoque iterativo
-tiempo_iterativo = timeit.timeit('factorial_iterativo(10)', globals=globals(), number=1000)
+tiempo_iterativo = timeit.timeit('factorial_iterativo(10)', globals=globals(), number=10000)
 # Tiempo del enfoque iterativo
-tiempo_recursivo = timeit.timeit('factorial_recursivo(10)', globals=globals(), number=1000)
+tiempo_recursivo = timeit.timeit('factorial_recursivo(10)', globals=globals(), number=10000)
+
+print(f"Tiempo Iterativo: {tiempo_iterativo} segundos")
+print(f"Tiempo Recursivo: {tiempo_recursivo} segundos")
+
+# Tiempo del enfoque iterativo
+tiempo_iterativo = timeit.timeit('factorial_iterativo(500)',globals=globals(), number=100)
+# Tiempo del enfoque recursivo
+tiempo_recursivo = timeit.timeit('factorial_recursivo(500)',globals=globals(), number=100)
+
 print(f"Tiempo Iterativo: {tiempo_iterativo} segundos")
 print(f"Tiempo Recursivo: {tiempo_recursivo} segundos")
